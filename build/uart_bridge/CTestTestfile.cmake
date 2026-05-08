e086cb489b3c2576d8885095b23abd5b971225b4
Roi Bachynskyi <roybachynskyi@gmail.com>
2026-05-08 19:15:20 +0300

    init

---
+++ build/uart_bridge/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sleepy/catkin_ws/src/uart_bridge
+# Build directory: /home/sleepy/catkin_ws/build/uart_bridge
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
